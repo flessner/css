ac061962a65b47abbd6f8a0094c68f70606bbc69
flessner_ <dev@flessner.io>
2025-01-10 09:17:20 +0100

    added page shortcut and fixed var handling

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { Preset } from 'unocss'
 import { presetWind } from 'unocss'
 
+function resolveColor(x: string) {
+  return `var(--color-${x}, var(--${x}))`
+}
+
 function resolveOpacity(x: string) {
-  if (isNaN(Number(x))) {
-    return `var(--${x})`
-  } else {
-    return `${x}`
-  }
+  return `var(--opacity-${x}, var(--${x}))`
 }
 
 export function presetFlessner(): Preset {
@@ -16,15 +16,21 @@ export function presetFlessner(): Preset {
       presetWind(),
     ],
     rules: [
-      [/^bg-([\w]+)$/, ([, x]) => ({ 'background-color': `hsl(var(--${x}))` })],
-      [/^text-([\w]+)$/, ([, x]) => ({ 'color': `hsl(var(--${x}))` })],
+      [/^bg-([\w]+)$/, ([, x]) => ({ 'background-color': `hsl(${resolveColor(x)})` })],
+      [/^text-([\w]+)$/, ([, x]) => ({ 'color': `hsl(${resolveColor(x)})` })],
       // with opacity
-      [/^bg-([\w]+)[/]([\w]+)$/, ([, x, y]) => ({ 'background-color': `hsl(var(--${x}) / ${resolveOpacity(y)})` })],
-      [/^text-([\w]+)[/]([\w]+)$/, ([, x, y]) => ({ 'color': `hsl(var(--${x}) / ${resolveOpacity(y)})` })],
+      [/^bg-([\w]+)[/]([\w]+)$/, ([, x, y]) => ({ 'background-color': `hsl(${resolveColor(x)} / ${resolveOpacity(y)})` })],
+      [/^text-([\w]+)[/]([\w]+)$/, ([, x, y]) => ({ 'color': `hsl(${resolveColor(x)} / ${resolveOpacity(y)})` })],
     ],
     shortcuts: {
       'row': 'flex flex-row',
       'col': 'flex flex-col',
+      'page-xs': 'max-w-xs mx-auto px-4',
+      'page-sm': 'max-w-sm mx-auto px-4',
+      'page-md': 'max-w-md mx-auto px-4',
+      'page-lg': 'max-w-lg mx-auto px-4',
+      'page-xl': 'max-w-xl mx-auto px-4',
+      'page-2xl': 'max-w-2xl mx-auto px-4',
     },
   }
 }
